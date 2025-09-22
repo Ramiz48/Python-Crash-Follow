3ff6e6ba94274602b306409e4b00e41731080997
Ramiz <101509539+Ramiz48@users.noreply.github.com>
2025-09-22 18:56:11 +0500

    Update Python_Crash_With_Harry.py

--- Python_Crash_With_Harry.py
+++ Python_Crash_With_Harry.py
@@ -1,1 +1,49 @@
 print("hello world") 
+# Python Basics
+
+**Data variables:** 
+
+Boolean =  True or False
+
+String = “Ramiz” any series of characters
+
+Integers = numbers
+
+Float =  1.2, 3.4, 5.7, Basically numbers in decimals
+
+None = None (you can write None as a None and it gives no error which means its acceptable not like other string where you have to must put double comma’s.)
+
+**Type casting** : is basically converting anything into another one like string into int form.
+
+For Example:
+
+          A = “5”
+
+           print(A + 1) # in that scenario we have to first convert this into int like (int (5) +1)
+
+           B = 5
+
+           print (B +1 )
+
+**Operators:**
+
+**1: Arithmetic Operators:**
+
+num1 = 10
+
+num2 = 2
+
+print(”num1 + num2 is “, num1 + num2) 
+
+print(”num1 - num2 is “, num1 - num2) 
+
+print(”num1 * num2 is “, num1 * num2)
+
+print(”num1 /  num2 is “, num1 / num2)
+
+print(”num1 /  /  num2 is “, num1 /  / num2) # it eliminate numbers after decimal.
+
+print(”num1 ** num2 is “, num1 ** num2) # it gives us power like 10 power 10 it makes 1000
+
+print(”num1 % num2 is “, num1 % num2) # it gives us reminder like 10/1 and reminder will be 1
+
